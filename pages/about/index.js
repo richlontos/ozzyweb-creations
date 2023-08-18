b746e296b0d2c81c703cd8d05f97ec359e184fb2
richard lontos <richard.lontos95@gmail.com>
2023-08-17 20:44:05 -0700

    Key added

--- pages/about/index.js
+++ pages/about/index.js
@@ -208,48 +208,38 @@ const About = () => {
           exit='hidden'
           className='flex flex-col w-full xl:max-w-[48%] h-[480px]'
         >
-          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
-            {aboutData.map((item, itemIndex) => {
-              return (
-                <div
-                  key={item.title + itemIndex}
-                  className={`${index === itemIndex &&
-                    'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
-                    }  cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-                  onClick={() => setIndex(itemIndex)}
-                >
-                  {item.title}
-                </div>
-              );
-            })}
+           <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+            {aboutData.map((item, itemIndex) => (
+              <div
+                key={item.title + '-' + itemIndex}
+                className={`${index === itemIndex && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                onClick={() => setIndex(itemIndex)}
+              >
+                {item.title}
+              </div>
+            ))}
           </div>
           <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
-            {aboutData[index].info.map((item, itemIndex) => {
-              return (
-                <div
-                  key={item.title + itemIndex}
-                  className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
-                >
-                  {/* title */}
-                  <div className='font-light mb-2 md:mb-0'>{item.title}</div>
-                  <div className='hidden md:flex'>-</div>
-                  <div>{item.stage}</div>
-                  {/* Render the image if it exists */}
-                  {item.image && (
-                    <img src={item.image} alt={item.title} className='w-24 h-24 object-cover rounded-full' />
-                  )}
-                  <div className='flex gap-x-4'>
-                    {/* icons */}
-                    {item.icons?.map((icon, itemIndex) => {
-                      <div key={itemIndex} className='text-2xl text-white'> 
+            {aboutData[index].info.map((item, itemIndex) => (
+              <div key={item.title + '-' + itemIndex} className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'>
+                <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+                <div className='hidden md:flex'>-</div>
+                <div>{item.stage}</div>
+                {item.image && (
+                  // For optimization, consider using Next.js's <Image /> component as suggested by the linting warning
+                  <img src={item.image} alt={item.title} className='w-24 h-24 object-cover rounded-full' />
+                )}
+                <div className='flex gap-x-4'>
+                  {item.icons?.map((icon, iconIndex) => (
+                    <div key={'icon-' + iconIndex} className='text-2xl text-white'>
                       {icon}
                     </div>
-                    })}
-                  </div>
+                  ))}
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
+
         </motion.div>
       </div>
     </div>
